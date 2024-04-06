d3dd8ee7e9e1b6965176ade028787120e745f41f
mille055 <46400739+mille055@users.noreply.github.com>
2024-04-06 18:26:43 -0400

    new_app4 format better prompt logic worse

--- new_app4.py
+++ new_app4.py
@@ -12,6 +12,17 @@ rag = RAG()
 # Streamlit page configuration
 st.set_page_config(page_title="Duke AIPI Chatbot", layout="wide")
 
+def bot_message(message):
+    st.markdown(f'<div class="bot-message" style="display: flex; padding: 5px;">'
+                f'<div style="background-color: #012169; color: white; padding: 10px; border-radius: 10px; font-size:18px; margin-bottom:10px; margin-left:20px;">{message}</div>'
+                f'</div>', unsafe_allow_html=True)
+
+def user_message(message):
+    st.markdown(f'<div class="user-message" style="display: flex; padding: 5px;">'
+                f'<div style="background-color: #0577B1; color: white; padding: 10px; border-radius: 10px; font-size:18px; margin-bottom:10px; margin-left:20px;">{message}</div>'
+                f'</div>', unsafe_allow_html=True)
+
+
 def run_UI():
     # Apply custom CSS for styling message boxes
     st.markdown("""
@@ -41,6 +52,7 @@ def run_UI():
     avatar_assistant = 'assets/duke_d_2.png'
     
     st.image('assets/duke_chapel_blue_with_text.png', caption='Duke University')
+    
     # Initialize or retrieve the conversation history from the session state
     if 'conversation_history' not in st.session_state:
         st.session_state.conversation_history = []
@@ -49,20 +61,28 @@ def run_UI():
     for message in st.session_state.conversation_history:
         
         with st.chat_message(message["role"], avatar = message["avatar"]):
-            st.markdown(message["content"])
+            #st.markdown(message["content"])
+            if message["role"]=="user":
+                user_message(message["content"])
+            else:
+                bot_message(message["content"])
+
 
     if prompt := st.chat_input("What questions can I help you with?"):
         st.session_state.conversation_history.append({"role": "user", "content": prompt, "avatar": avatar_user})
         with st.chat_message("user", avatar = avatar_user):
-            st.markdown(prompt)
+            #st.markdown(prompt)
+            user_message(prompt)
 
         with st.chat_message("assistant", avatar=avatar_assistant):
-            
-            response_text, sources = rag.generate_response(prompt)
+            whole_prompt = prompt+ " ".join([message['content'] for message in st.session_state.conversation_history])
+            print(whole_prompt)
+            response_text, sources = rag.generate_response(whole_prompt)
             # Append user query and response to conversation history
             #st.session_state.conversation_history.append({"role": "user", "content": prompt})
             st.session_state.conversation_history.append({"role": "assistant", "content": response_text, "avatar": avatar_assistant})
-            response = st.write(response_text)
+            #response = st.write(response_text)
+            bot_message(response_text)
             if sources:
                 st.markdown(f"<div style='text-align: right;'><a href='{sources[0]}' target='_blank'><button style='background-color: #4CAF50; color: white; padding: 10px 24px; margin: 10px; border: none; border-radius: 12px; cursor: pointer;'>Learn More</button></a></div>", unsafe_allow_html=True)
 
